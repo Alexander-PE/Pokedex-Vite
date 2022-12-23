6c8f6427fc72c97d1eed830ce668ae85a091af1b
Alexander Perez <alexander.pereze02@gmail.com>
2022-12-22 21:27:33 -0500

    First commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        'laurel':"#B9BAA3",
+        'davys': '#565656'
+      }
+    },
+  },
+  plugins: [],
+}
